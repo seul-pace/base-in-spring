fab81e28c2adcff040cec9d2b87b3bd00ca234a6
seulhui <lsh9412@naver.com>
2024-05-31 23:04:17 +0900

    Init

---
+++ src/main/java/base/base_in_spring/BaseInSpringApplication.java
@@ -0,0 +1,13 @@
+package base.base_in_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseInSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseInSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/base/base_in_spring/BaseInSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package base.base_in_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseInSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
